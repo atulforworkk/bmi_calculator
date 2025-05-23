e77e9673d0f7aaa12fa6ce32956003f59e36c63b
akt <akt@akts-MacBook-Air.local>
2025-05-23 16:24:42 +0530

    new changes

--- src/views/page/home/Home.tsx
+++ src/views/page/home/Home.tsx
@@ -2,7 +2,7 @@ import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { Input } from "@mantine/core";
 import { Button } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // Example Validation Schema
 const ValidationSchema = Yup.object().shape({
   weight: Yup.number().required("Weight is required"),
@@ -10,7 +10,21 @@ const ValidationSchema = Yup.object().shape({
 });
 
 export default function BMICalculator() {
-  const [bmi,setBmi]=useState("");
+  const [bmi, setBmi] = useState<number>(0);
+  console.log("bmi",bmi);
+  const [bmiCategories, setBmiCategories] = useState("");
+  useEffect(() => {
+    if (bmi === undefined) return;
+    if (bmi < 18.5) {
+      setBmiCategories("You're Underweight");
+    } else if (bmi < 25) {
+      setBmiCategories("You're Normal");
+    } else if (bmi < 30) {
+      setBmiCategories("You're Overweight");
+    } else {
+      setBmiCategories("You're Obese");
+    }
+  }, [bmi]);
   return (
     <div className="flex flex-col h-[80vh] items-center justify-center">
       <div className="flex flex-col w-4/12">
@@ -21,7 +35,7 @@ export default function BMICalculator() {
             const heightInMeters = Number(values.height) / 100;
             const weight = Number(values.weight);
             const bmi = weight / (heightInMeters * heightInMeters);
-            setBmi(bmi.toFixed(2));
+            setBmi(parseFloat(bmi.toFixed(2)));
             console.log("BMI:", bmi.toFixed(2));
           }}
         >
@@ -43,7 +57,10 @@ export default function BMICalculator() {
         </Formik>
       </div>
       <div>
-        <p className="mt-8">Your BMI is {bmi}</p>
+        <p className="mt-8 text-xl font-semibold">Your BMI is {bmi}</p>
+      </div>
+      <div>
+        <p className="mt-4 text-xl font-semibold">{bmiCategories}</p>
       </div>
     </div>
   );
